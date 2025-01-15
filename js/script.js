5da7d02d31e0265d5d8d333f4f363ef9d64416a9
Alex <example@gmail.com>
2025-01-15 19:36:36 +0200

    code exercise 6

--- js/script.js
+++ js/script.js
@@ -1,6 +1,22 @@
 'use strict';
 
-const isChecked = true;
-const isClose = true;
-
-console.log()
+//* 1
+function getName(name) {
+    return `Привет, ${name}`;
+}
+//* 2
+function returnNeighboringNumbers(num) {
+    return [num - 1, num, num + 1];
+}
+//* 3
+function getMathResult(a, b) {
+    if (typeof(b) !== "number" || b <= 0) return a;
+    let result = "";
+    let num = a;
+    for (let i = 0; i < b; i++) {
+        result += (i === 0) ? num : `---${num}`;
+        num += a;
+    }
+    return result;
+}
+console.log(getMathResult(10, 5));
